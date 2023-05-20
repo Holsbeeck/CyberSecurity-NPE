cb9e3861d16e950a19491d263f84063002b020f4
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2023-05-20 15:12:29 +0200

    Update RoxyInstallation.sh

--- RoxyInstallation.sh
+++ RoxyInstallation.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Voorbereidingen voor Roxy installatie
-sudo apt install apache2 
+sudo apt install apache2 curl -y
 sudo mkdir /var/www/haproxy-wi
-sudo apt install curl -y
 curl -o v6.1.0.0.tar.gz -L https://github.com/hap-wi/roxy-wi/archive/refs/tags/v6.1.0.0.tar.gz
 sudo tar -xzf v6.1.0.0.tar.gz -C "/var/www/haproxy-wi" --strip-components=1
 rm v6.1.0.0.tar.gz
