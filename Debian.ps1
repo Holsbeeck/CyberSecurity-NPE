bd48faa4e6807a97dcb10f6508b0909c4150625c
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2023-05-20 12:48:56 +0200

    Push van werkend VM script (tested)

--- Debian.ps1
+++ Debian.ps1
@@ -1,24 +1,31 @@
-# Aanmaken van variable, herbruiken script (Kali & Debian) + mogelijk maken van path changes
+$naam1 = "NPEDebian"
+$naam2 = "NPEKali"
+$router = "Debain Router"
 
-$naam = "NPEDebian"
-$vdi = "C:\Users\Jelle\Desktop\debian.vdi" 
+# De hostonlyadapter met onze debian router 
+# vboxmanage list hostonlyifs 
+$adapt = "VirtualBox Host-Only Ethernet Adapter #3"
 
-# Script om de VM aan te maken, bekijken ostype - cores - mem 
+# Path aanpassen naar locatie van de VDI files, 1 voor Debian en 2 voor Kali!
+$vdi1 = "C:\Users\Jelle\Desktop\debian.vdi" 
+$vdi2 = "C:\Users\Jelle\Desktop\kali.vdi"
 
-vboxmanage createvm --name "$naam" --ostype Debian_64 --register
-vboxmanage modifyvm "$naam" --memory 2048 --cpus 2 --nic1 nat
+# UUID aanpassen om conflict te voorkomen met al bestaande HDD 
+# VBoxManage internalcommands sethduuid "vdi2"
 
-vboxmanage storagectl "$naam" --name "SATA Controller" --add sata
-vboxmanage storageattach "$naam" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$vdi"
+# Gedeelte voor Debian(machine 1) 2 cores om kernel panic te vermijden met 2GB aan RAM. 
+vboxmanage createvm --name "$naam1" --ostype Debian_64 --register
+vboxmanage modifyvm "$naam1" --memory 2048 --cpus 2 --nic1 hostonly --hostonlyadapter1 "$adapt"
+vboxmanage storagectl "$naam1" --name "SATA Controller" --add sata
+vboxmanage storageattach "$naam1" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$vdi1"
 
-vboxmanage storagectl "$naam" --name "IDE Controller" --add ide
-vboxmanage storageattach "$naam" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "C:\Program files\Oracle\VirtualBox\VBoxGuestAdditions.iso"
+# Gedeelte voor Kali(machine 2) 4 cores 4GB aan RAM.
+vboxmanage createvm --name "$naam2" --ostype Debian_64 --register
+vboxmanage modifyvm "$naam2" --memory 4096 --cpus 4 --nic1 hostonly --hostonlyadapter1 "$adapt"
+vboxmanage storagectl "$naam2" --name "SATA Controller" --add sata
+vboxmanage storageattach "$naam2" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$vdi2"
 
-# VM te starten voor Vbox additions 
-
-vboxmanage startvm "$naam"
-# Start-Sleep -Seconds 30
-# vboxmanage guestcontrol "$naam" run --exe /mnt/VBoxGuestAdditions/VBoxLinuxAdditions.run --username "osboxes" --password "osboxes.org"
-# vboxmanage controlvm "$naam" poweroff
-
-# vboxmanage storageattach "$naam" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium emptydrive
+# Machines opstarten (debian router optie kan weggelaten worden)
+vboxmanage startvm "$naam1"
+vboxmanage startvm "$naam2"
+vboxmanage startvm "$router"
---
+++ RoxyPreparation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Voorbereidingen voor Roxy installatie
+sudo apt-get install curl 
+sudo mkdir /var/www/haproxy-wi
+
+# Download 
+curl -o v6.1.0.0.tar.gz -L https://github.com/hap-wi/roxy-wi/archive/refs/tags/v6.1.0.0.tar.gz
+
+# Extract 
+tar -xzf v6.1.0.0.tar.gz -C "/var/www/haproxy-wi" --strip-components=1
+
+# Clean up
+rm v6.1.0.0.tar.gz
+
+echo "Roxy-WI repository is succesvol binnengehaald"
